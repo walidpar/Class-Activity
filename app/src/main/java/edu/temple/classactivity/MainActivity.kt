dc056faaacd6dee9b4b7e79413ec16846f39c70f
Walid Parast <walidparast1@gmail.com>
2025-01-28 12:13:45 -0500

    Added a reaction for the button, now it says "Button Clicked" when pressed.

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,15 +1,22 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        (findViewById(R.id.button) as Button).setOnClickListener {
+            (it as Button).text = "Button CLicked!"
+        }
     }
 }
